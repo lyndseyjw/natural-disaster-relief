ece664dec7c7e91050c93cebed328951f4a6285c
Nick_Kash <nicholas.kasdjono.777@my.csun.edu>
2021-08-24 19:13:39 -0700

    added html & css files

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ var zipSubmit = $('.zipSubmitButton');
 var zipInputEl = $('<div>');
 
 // variable created for the zip form input value (to display in the created div)
-var zipInput = input.val();
+//    var zipInput = input.val();
 
 // creating a variable for the container of the map
 var mapDiv = $('.mapDiv');
@@ -15,3 +15,15 @@ var disasterInfoEl = $('<div>');
 
 // creating a variable for the API response of disaster info
 var disasterInfo = $('.disasterInfo');
+
+
+
+// ------ Timer @ top of webpage ------ //
+
+var liveTime = document.querySelector(".timer");
+var timer = setInterval(function () {
+
+    var currTime = moment().format("dddd, MMMM Do YYYY, h:mm:ss");
+    liveTime.textContent = currTime;
+
+}, 1000); // 10000ms = 1s
