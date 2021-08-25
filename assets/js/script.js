cd102de5233b1078a2007f3684cf479975d313bd
lyndseyjw <lyndseywatson@Lyndseys-MacBook-Pro.local>
2021-08-25 15:21:44 -0700

    connected ambee & position stack APIs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,53 +1,73 @@
-// // creating a variable for the zipcode Submit button in HTML
-// var zipSubmit = $('.zipSubmitButton');
+// variable for the search container, where the locally saved zip code will display
+var searchDiv = $('.searchDiv');
 
-// var searchDiv = $('.searchDiv');
+// variable created for the zip form input value (to display in the created div)
+var zipInput = $('.zipInput');
 
-// // a div is created underneath zip form
-// var zipInputEl = $('<div>');
+// creating a variable for the zipcode Submit button in HTML
+var zipSubmit = $('.zipSubmitButton');
 
-// // variable created for the zip form input value (to display in the created div)
+// creating a variable for the container of the map
+var mapDiv = $('.mapDiv');
 
-// var zipInput = $('.zipInput');
-// zipInput = input.val();
+// creating a variable for the container that will house the API response (air quality & road closures)
+var disasterInfoEl = $('<div>');
 
-// //    var zipInput = input.val();
+// creating a variable for the API response of disaster info
+var disasterInfo = $('.disasterInfo');
 
-// // creating a variable for the container of the map
-// var mapDiv = $('.mapDiv');
+var latitude;
+var longitude;
+var fireLatitude;
+var fireLongitude;
+var map;
 
-// // creating a variable for the container that will house the API response (air quality & road closures)
-// var disasterInfoEl = $('<div>');
+var liveTime = document.querySelector(".timer");
+var timer = setInterval(function () {
 
-// // creating a variable for the API response of disaster info
-// var disasterInfo = $('.disasterInfo');
+    var currTime = moment().format("dddd, MMMM Do YYYY, h:mm:ss");
+    liveTime.textContent = currTime;
 
-// // zipSubmit.on('click', function()) {
+}, 1000);
 
+zipSubmit.on('click', function () {
 
+    var zipInputVal = zipInput.val();
 
+    var positionStackURL = 'http://api.positionstack.com/v1/forward?access_key=504536cca90d4c48fb032176b5240b9c&query=' + zipInputVal
 
+    fetch(positionStackURL)
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (data) {
+            console.log(data);
 
-// // ------ Timer @ top of webpage ------ //
+            latitude = data.data[0].latitude;
+            longitude = data.data[0].longitude;
+            console.log(latitude);
+            console.log(longitude);
 
-// var liveTime = document.querySelector(".timer");
-// var timer = setInterval(function () {
+            fetch("https://api.ambeedata.com/latest/fire?lat=" + latitude + "&lng=" + longitude, {
+                "method": "GET",
+                "headers": {
+                    "x-api-key": "bcdc320dee6e51c49f7af1f5a7d6cdb150c47a4475a4df5fc55f94fcbd7b6595",
+                    "Content-type": "application/json"
+                }
+            })
+                .then(function (response) {
+                    return response.json()
+                })
+                .then(function (data) {
+                    console.log(data);
+                })
+                    fireLatitude = data.data[0].latitude;
+                    fireLongitude = data.data[0].longitude;
+                    console.log(fireLatitude);
+                    console.log(fireLongitude);
 
-//     var currTime = moment().format("dddd, MMMM Do YYYY, h:mm:ss");
-//     liveTime.textContent = currTime;
+                    mapDiv.attr('href', '');
+                    map = L.map(mapDiv).setView([latitude, longitude], 13);
+        })
 
-// }, 1000); // 10000ms = 1s
-
-fetch("https://api.ambeedata.com/latest/fire?lat=12.9889055&lng=77.574044", {
-	"method": "GET",
-	"headers": {
-		"x-api-key": " bcdc320dee6e51c49f7af1f5a7d6cdb150c47a4475a4df5fc55f94fcbd7b6595",
-		"Content-type": "application/json"
-	}
 })
-.then(response => {
-	console.log(response);
-})
-.catch(err => {
-	console.error(err);
-});
