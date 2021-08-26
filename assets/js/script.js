c47fb50b1e0e4726e46c8b12b9475525bdea2011
lyndseyjw <lyndseywatson@Lyndseys-MacBook-Pro.local>
2021-08-26 13:41:46 -0700

    added the fire icon

--- assets/js/script.js
+++ assets/js/script.js
@@ -74,16 +74,18 @@ zipSubmit.on('click', function () {
                     }).addTo(map);
 
                     fireIcon = L.icon({
-                        
-                        iconUrl: 'leaf-green.png',
-                        shadowUrl: 'leaf-shadow.png',
+
+                        iconUrl: './assets/iamges/fire-emoji.png',
+                        // shadowUrl: 'leaf-shadow.png',
                     
-                        iconSize:     [38, 95], // size of the icon
-                        shadowSize:   [50, 64], // size of the shadow
+                        iconSize:     [95, 95], // size of the icon
+                        // shadowSize:   [50, 64], // size of the shadow
                         iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-                        shadowAnchor: [4, 62],  // the same for the shadow
+                        // shadowAnchor: [4, 62],  // the same for the shadow
                         popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
                     });
+
+                    L.marker([fireLatitude, fireLongitude], {icon: fireIcon}).addTo(map);
         })
 
 })
