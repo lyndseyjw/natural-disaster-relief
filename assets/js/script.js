1278b47098a6624d5b61a09f68ae6f51b8ca0bb3
stephenryan183 <stephenryan183@gmail.com>
2021-08-26 11:49:43 -0700

    changed emoji

--- assets/js/script.js
+++ assets/js/script.js
@@ -79,9 +79,9 @@ zipSubmit.on('click', function () {
                         shadowUrl: 'leaf-shadow.png',
                     
                         iconSize:     [38, 95], // size of the icon
-                        shadowSize:   [50, 64], // size of the shadow
+                        // shadowSize:   [50, 64], // size of the shadow
                         iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-                        shadowAnchor: [4, 62],  // the same for the shadow
+                        // shadowAnchor: [4, 62],  // the same for the shadow
                         popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
                     });
         })
